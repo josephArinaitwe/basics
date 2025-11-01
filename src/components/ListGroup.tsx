e4dd4ee6602f4becd7549e04cfb9a054b4562b3e
josephArinaitwe <jarinaitwe@kiiramotors.com>
2025-11-01 21:05:12 +0300

    initial commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,35 @@
+import {MouseEvent} from 'react';
+function ListGroup() {
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Mumbai'
+    ]
+
+    const handleClick = (event: MouseEvent) => {
+        console.log(event);
+    }
+
+    const getMesssage = () =>{
+        return items.length === 0 ? 'No items found' : 'showing ' + items.length + ' items';
+    }
+
+  return (
+    <> 
+    <h1>List Group</h1> 
+    { items.length === 0 && 'No items found' }  
+   
+    <ul className="list-group">
+      {items.map(item => 
+          <li key={item} 
+          className="list-group-item" 
+          onClick={() => console.log('clicked')}> {item}</li>
+      )}
+    </ul>
+
+    </>
+  );
+}
+export default ListGroup;
