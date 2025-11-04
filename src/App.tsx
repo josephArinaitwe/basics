32ad3075517679f1ffbe8b6444158b0640f0c5aa
josephArinaitwe <jarinaitwe@kiiramotors.com>
2025-11-04 16:05:39 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,62 +10,105 @@ function App(){
   // };
   // return (
   //   <div>
-  //     <ListGroup 
+  //     <ListG3roup 
   //       items={items} 
   //       heading="Cities"
   //       onSelectItem={handleSelectItem} />
         
   //   </div>
   // )
-  const [showAlert, setShowAlert] = useState(false);
-  const [drink, setDrink] = useState({
-    name: 'coffee',
-    price: 5
-  })
+//   const [showAlert, setShowAlert] = useState(false);
+//   const [drink, setDrink] = useState({
+//     name: 'coffee',
+//     price: 5
+//   })
 
-  const [person, setPerson] = useState(
-    {firstname: 'John', lastname: 'Doe'}
-  )
+//   const [person, setPerson] = useState(
+//     {firstname: 'John', lastname: 'Doe'}
+//   )
   
-  const handleClick = () => {
-    setShowAlert(true);
-  }
+//   const handleClick = () => {
+//     setShowAlert(true);
+//   }
 
-  const handleCloseAlert = () => {
-    setShowAlert(false);
-  }
+//   const handleCloseAlert = () => {
+//     setShowAlert(false);
+//   }
 
-  const handleUpdateDrink = () => {
-    setDrink({name: 'Latte', price: 6});
-  }
+//   const handleUpdateDrink = () => {
+//     setDrink({...drink, price: 10, name:'Mocha'});
+//   }
 
-return( 
-<div >
+// return( 
+// <div >
 
-  <ListGroup items={['New York', 'San Francisco', 'Tokyo', 'London', 'Mumbai']} heading="Cities" onSelectItem={(item) => console.log(item)} />
+//   <ListGroup items={['New York', 'San Francisco', 'Tokyo', 'London', 'Mumbai']} heading="Cities" onSelectItem={(item) => console.log(item)} />
     
-    {/* <Button onClick={() => console.log('Button clicked!')} color="success">Click Me</Button > */}
-    { showAlert && 
-      <Alert onClose={handleCloseAlert}>
-        This is an <span className='alert-link'> alert </span> message!
-      </Alert>
-    }
-    <Button onClick={handleClick} color="warning">Click Me</Button >
+//     {/* <Button onClick={() => console.log('Button clicked!')} color="success">Click Me</Button > */}
+//     { showAlert && 
+//       <Alert onClose={handleCloseAlert}>
+//         This is an <span className='alert-link'> alert </span> message!
+//       </Alert>
+//     }
+//     <Button onClick={handleClick} color="warning">Click Me</Button >
 
-    <Message />
-     <Message />
-      <Message />
-       <Message />
-      <div>{drink.name} : ${drink.price}</div>
-       <button onClick={handleUpdateDrink}>Update Drink</button>
-       <form action=""></form>
-       <form action="inpu" className="border border-black"></form>
+//     <Message />
+//      <Message />
+//       <Message />
+//        <Message />
+//       <div>{drink.name} : ${drink.price}</div>
+//        <button onClick={handleUpdateDrink}>Update Drink</button>
+//        <form action=""></form>
+//        <form action="inpu" className="border border-black"></form>
 
-  </div>
-  );
+//   </div>
+//   );
+const [customer, setCustomer] = useState({
+  name: 'john',
+  address: {
+    city: 'New York',
+    street: '5th Avenue',
+    zipcode: '10001'
+  }
+});
+const [tags, setTags] = useState<string[]>(['tag1', 'tag2', 'tag3']);
 
+const handleTags = () => {
+  console.log(tags);
+  setTags([...tags, 'tag4', 'tag5', 'tag6']);
 
- 
+  setTags(tags.filter(tag => tag !== 'tag1'))
 }
 
-export default App;
+
+ const handleUpdate = () => {
+   {console.log(customer)}
+    setCustomer({
+      ...customer, 
+      name: 'Jane',
+      address: {
+        ...customer.address,
+        city: 'London'
+      }
+    })
+  }
+return (
+  <div>
+   <button onClick={handleUpdate}>
+    Update Customer
+   </button>
+
+  <button onClick={handleTags}>
+    Update Tags
+  </button>
+
+  
+  </div>
+
+);
+
+
+
+}
+
+export default App
