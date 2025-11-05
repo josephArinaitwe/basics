c63cb5122dfd34afaa8f504211666788e001a2ef
josephArinaitwe <jarinaitwe@kiiramotors.com>
2025-11-05 10:11:07 +0300

    state management final

--- src/App.tsx
+++ src/App.tsx
@@ -67,14 +67,71 @@ function App(){
 //   </div>
 //   );
 //
-const [cartItems, setCarItems] = useState([
-  'product1', 'product2', 'product3'
-]);
-return(
-<>
-<Navbar cartItemsCount={cartItems.length} />
-<CartItem cartItems={cartItems} onClear={() =>setCarItems([])}/>
-</>
+// const [cartItems, setCarItems] = useState([
+//   'product1', 'product2', 'product3'
+// ]);
+// return(
+// <>
+// <Navbar cartItemsCount={cartItems.length} />
+// <CartItem cartItems={cartItems} onClear={() =>setCarItems([])}/>
+// </>
+// )
+// const [game, setGame] = useState ({
+//   id: 1,
+//   player: {
+//     name: 'John',
+//     score: 0
+//   }
+// })
+// const handleClick = () => {
+//   setGame({...game, player: {...game.player, name: 'David'}})
+// }
+
+
+// return (
+//   <>
+//   {game.player.name} : {game.player.score}
+//   <button onClick={handleClick}>Change Name</button>
+
+//   </>
+// )
+const [pizza, setPizza] = useState({
+  name: 'Pepperoni',
+  toppings: ['Mushrooms']
+})
+
+const [cart, setCart] = useState({
+  discount: .1,
+  items: [
+    {id: 1, title: 'Product 1', quantity: 1},
+    {id: 2, title: 'Product 2', quantity: 2},
+    {id: 3, title: 'Product 3', quantity: 1}
+  ]
+})
+
+const handleClick = () => {
+  setPizza({...pizza, toppings: [...pizza.toppings, 'onions']})
+}
+
+const handleChangeQty = () =>{
+  setCart({...cart, items: cart.items.map(item => item.id === 2 ? {...item, quantity: 5} : item)})
+}
+return (
+  <>
+  {pizza.name} : {pizza.toppings.join(', ')}
+  <button onClick={handleClick}>Add Topping</button>
+
+  <ul>
+    {cart.items.map(item => 
+      <li key={item.id}>
+        {item.title} : {item.quantity}
+      </li>
+    )}
+    <button onClick={handleChangeQty}>Change Quantity</button>
+  </ul>
+
+
+  </>
 )
 }
 export default App;
